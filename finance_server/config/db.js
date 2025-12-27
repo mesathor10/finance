964904c6411421c401ce8b8b337d83802bec3746
Sathish Kumar <101390150+mesathor10@users.noreply.github.com>
2025-12-27 07:02:06 +0530

    Update db.js

--- finance_server/config/db.js
+++ finance_server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 function dbConnection() {
   mongoose
-    .connect("mongodb://localhost:27017/finance")
+    .connect("mongodb+srv://sathishkumar4118:mesathor10@financecluster.ukshbic.mongodb.net/")
     .then((res) => console.log("Database Connected"))
     .catch((err) => console.log("Database doesn't connected", err));
 }
